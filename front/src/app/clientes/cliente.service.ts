8e7d88ded7dd4f5f52ae6e8db44500e59b902472
Leonardo <91767116+leo2143@users.noreply.github.com>
2024-01-06 14:11:38 -0300

    se agrega alerta de creacion de cliente

--- front/src/app/clientes/cliente.service.ts
+++ front/src/app/clientes/cliente.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs';
-import { of } from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
+
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
   private UrlEndpoint: string = 'http://localhost:8080/api/clientes'
   private httpHeaders = new HttpHeaders({'content-type':'application/json'})
-
+  
   constructor(private http: HttpClient) {}
 
   getClientes(): Observable<Cliente[]>{
--- front/src/app/clientes/form.component.ts
+++ front/src/app/clientes/form.component.ts
@@ -2,6 +2,8 @@ import { Cliente } from './cliente';
 import { Component, OnInit } from '@angular/core';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-form',
@@ -17,7 +19,26 @@ export class FormComponent implements OnInit {
   //este metodo se suscribe al service el cual tiene la logica de creacion que llama al backend
   public create(): void{
     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
+      response => {
+        this.router.navigate(['/clientes']);
+        Swal.fire({
+          title: 'Nuevo cliente creado con éxito!',
+          text: `El cliente ${response} ha sido creado.`,
+          icon: 'success',
+          confirmButtonColor: '#C64EB2',
+          confirmButtonText: 'Aceptar'
+        });
+      },
+      error => {
+        Swal.fire({
+          title: 'Hubo un error al crear la persona',
+          text: ``,
+          icon: 'error',
+          confirmButtonText: 'Cool'
+
+        });
+
+      }
     )
   }
 }
